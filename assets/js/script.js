d801e14397a6e4cc336a87cc33c143d45bdf94ab
Brandon Evans <brandon.evans0708@gmail.com>
2021-06-12 16:31:43 -0600

    added code to try and copare times to change class

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,4 +45,26 @@ $("#task").on("blur", "textarea", function() {
    $(this).replaceWith(taskP);
 });
 
-displayTime()
+var auditTask = function(taskEl) {
+   // get date from task element
+   var date = $(taskEl).find("span").text().trim();
+ 
+   // convert to moment object at 5:00pm
+   var time = moment(date, "L").set("hour", 17);
+ 
+   // remove any old classes from element
+   // $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+ 
+   // apply new class if task is near/over due date
+   if (moment().isAfter(time)) {
+     $(taskEl).addClass("list-group-item-danger");
+   }else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+ };
+ 
+ setInterval(function () {
+    $(".card .list-group-item").each(function(index, el) {
+      auditTask(el);
+    });
+  }, (1000 * 60) * 30);
